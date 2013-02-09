ebc55d3d4b68e6ea30dbb2521846a963fd3a293e
plmaunz <plmaunz@sandia.gov>
2013-02-08 18:12:39 -0700

    work on PulseProgramUi

--- PulseProgramUi.py
+++ PulseProgramUi.py
@@ -7,12 +7,53 @@ Created on Thu Feb 07 22:55:28 2013
 
 import PyQt4.uic
 from PyQt4 import QtCore, QtGui
-
+import PulseProgram
+import re
 
 PulseProgramWidget, PulseProgramBase = PyQt4.uic.loadUiType('ui/PulseProgram.ui')
 
-class PulseProgramUi(PulseProgramWidget):
-    pass
+class PulseProgramUi(PulseProgramWidget,PulseProgramBase):
+    def __init__(self):
+        PulseProgramWidget.__init__(self)
+        PulseProgramBase.__init__(self)
+        self.pulseProgram = PulseProgram.PulseProgram()  
+        self.sourceTextEdits = dict()
+        
+        
+    def setupUi(self, parent):
+        super(PulseProgramUi,self).setupUi(parent)
+        self.loadButton.clicked.connect( self.onLoad )
+        self.saveButton.clicked.connect( self.onSave )
+        self.applyButton.clicked.connect( self.onApply )
+
+    def onSave(self):
+        print "onSave"
+        pass
+    
+    def onLoad(self):
+        print "onLoad"
+        fname = QtGui.QFileDialog.getOpenFileName(self, 'Open Pulse Programmer file')
+        print fname
+        self.pulseProgram.loadSource(str(fname))
+        self.showData()
+   
+    def onApply(self):
+        print "onApply"
+        pass
+
+    def showData(self):
+        # make the right number of tabs and show the source files
+        self.sourceTabs.clear()
+        self.sourceTextEdits = dict()
+        for name, text in self.pulseProgram.source.iteritems():
+            textEdit = QtGui.QTextEdit()
+#            print text
+#            text = re.sub( r"\r\n", r"\n", text)
+#            print text
+            textEdit.setPlainText(text)
+            self.sourceTextEdits[name] = textEdit
+            self.sourceTabs.addTab( textEdit, name )
+  
 
 if __name__ == "__main__":
     import sys
@@ -20,7 +61,7 @@ if __name__ == "__main__":
     MainWindow = QtGui.QMainWindow()
     widget = QtGui.QWidget()
     ui = PulseProgramUi()
-    ui.setupUi(widget)
-    MainWindow.setCentralWidget(widget)
+    ui.setupUi(ui)
+    MainWindow.setCentralWidget(ui)
     MainWindow.show()
     sys.exit(app.exec_())
