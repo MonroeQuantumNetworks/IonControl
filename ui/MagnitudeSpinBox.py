ea4d670a2f996b5a5b7b95066eeb29a5e4fa34ee
pmaunz <peter@maunz.name>
2013-02-10 22:40:12 -0700

    MagnitudeSpinBox

--- ui/MagnitudeSpinBox.py
+++ ui/MagnitudeSpinBox.py
@@ -7,15 +7,29 @@ Created on Sat Feb 09 17:28:11 2013
 
 from PyQt4 import QtGui, QtCore
 import re
+import PyQt4.uic
 import magnitude
+import sys
+import os.path
+sys.path.append(os.path.abspath(r'..\modules'))
+import Expression
+
+debug = False
 
 class MagnitudeSpinBox(QtGui.QAbstractSpinBox):
+    def __init__(self,parent=0):
+        super(MagnitudeSpinBox,self).__init__(parent)
+        self.expression = Expression.Expression()
+        
     def validate(self, inputstring, pos):
         print "validate"
-        m = re.match("\s*([-+0-9.]+)\s*(\w*)\s*",str(inputstring))
-        if m:
+#        m = re.match("\s*([-+0-9.]+)\s*(\w*)\s*",str(inputstring))
+        try:
+            self.expression.evaluate(str(inputstring))
             return (QtGui.QValidator.Acceptable,pos)
-        return (QtGui.QValidator.Intermediate,pos)
+        except Exception as e:
+            print e
+            return (QtGui.QValidator.Intermediate,pos)
         
     def stepBy(self, steps ):
         print steps
@@ -29,3 +43,33 @@ class MagnitudeSpinBox(QtGui.QAbstractSpinBox):
     def stepEnabled(self):
         print "stepEnabled"
         return QtGui.QAbstractSpinBox.StepUpEnabled | QtGui.QAbstractSpinBox.StepDownEnabled
+        
+    def value(self):
+        value = self.expression.evaluate( str( self.lineEdit().text() ))
+        print value
+        return value
+        
+if __name__ == "__main__":
+    debug = True
+    TestWidget, TestBase = PyQt4.uic.loadUiType('MagnitudeSpinBoxTest.ui')
+
+    class TestUi(TestWidget,TestBase):
+        def __init__(self):
+            TestWidget.__init__(self)
+            TestBase.__init__(self)
+        
+        def setupUi(self,parent):
+            super(TestUi,self).setupUi(parent)
+            self.updateButton.clicked.connect(self.onUpdate)
+            
+        def onUpdate(self):
+            self.lineEdit.setText( str(self.magnitudeSpinBox.value()) )
+
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    MainWindow = QtGui.QMainWindow()
+    ui = TestUi()
+    ui.setupUi(ui)
+    MainWindow.setCentralWidget(ui)
+    MainWindow.show()
+    sys.exit(app.exec_())
