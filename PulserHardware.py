0b8853e78769b48afc1724f8559c08ce33a12f58
pmaunz <peter@maunz.name>
2013-02-13 23:13:27 -0700

    bugfixes

--- PulserHardware.py
+++ PulserHardware.py
@@ -11,7 +11,7 @@ class PulserHardware(object):
         self._shutter = 0
         self._trigger = 0
         self.xem = xem
-        self.Mutex = QtCore.QMutex()
+        self.Mutex = QtCore.QMutex(QtCore.QMutex.Recursive)
         
     def setHardware(self,xem):
         self.xem = xem
@@ -37,10 +37,10 @@ class PulserHardware(object):
     def trigger(self,value):
         print "Trigger out not implemented"
         
-    def ppUpload(self,binarycode):
+    def ppUpload(self,binarycode,startaddress=0):
         with QtCore.QMutexLocker(self.Mutex):
             print "starting PP upload"
-            check( self.xem.SetWireInValue(0x00, 0, 0x0FFF), "ppUpload write start address" )	# start addr at zero
+            check( self.xem.SetWireInValue(0x00, startaddress, 0x0FFF), "ppUpload write start address" )	# start addr at zero
             self.xem.UpdateWireIns()
             check( self.xem.ActivateTriggerIn(0x41, 1), "ppUpload trigger" )
             print "Databuf length" ,len(binarycode)
@@ -53,12 +53,13 @@ class PulserHardware(object):
             return True
             
     def ppDownload(self,startaddress,length):
-        self.xem.SetWireInValue(0x00, startaddress, 0x0FFF)	# start addr at 3900
-        self.xem.UpdateWireIns()
-        self.xem.ActivateTriggerIn(0x41, 0)
-        data = bytearray('\000'*length)
-        num = self.xem.ReadFromPipeOut(0xA0, data)
-        return num, data
+        with QtCore.QMutexLocker(self.Mutex):
+            self.xem.SetWireInValue(0x00, startaddress, 0x0FFF)	# start addr at 3900
+            self.xem.UpdateWireIns()
+            self.xem.ActivateTriggerIn(0x41, 0)
+            data = bytearray('\000'*length)
+            num = self.xem.ReadFromPipeOut(0xA0, data)
+            return num, data
         
     def ppIsRunning(self):
         with QtCore.QMutexLocker(self.Mutex):
@@ -98,16 +99,14 @@ class PulserHardware(object):
     def ppReadData(self,minbytes,timeout=0.5):
         with QtCore.QMutexLocker(self.Mutex):
             self.xem.UpdateWireOuts()
-            byteswaiting = self.xem.GetWireOutValue(0x25) & 0x7ff  # pipe_out_available
+            byteswaiting = self.xem.GetWireOutValue(0x25) & 0xfff  # pipe_out_available
             tries = 0
             while byteswaiting<minbytes and tries<10:
                 time.sleep(timeout/10)
                 tries +=1
-                byteswaiting = self.xem.GetWireOutValue(0x25) & 0x7ff  # pipe_out_available
-            print hex(byteswaiting)
+                byteswaiting = self.xem.GetWireOutValue(0x25) & 0xfff  # pipe_out_available
             data = bytearray('\x00'*byteswaiting)
             self.xem.ReadFromPipeOut(0xa2, data)
-            print "Read {0} bytes".format(len(data))
             return data
             
     def ppReadLog(self):
@@ -117,16 +116,6 @@ class PulserHardware(object):
             self.xem.ReadFromPipeOut(0xA1, data)
             with open(r'debug\log','wb') as f:
                 f.write(data)
-
-#            if ((data[:2] != '\xED\xFE') or (data[-2:] != '\xED\x0F')):
-#                print "Bad data string: ", map(ord, data)
-#                return True
-
-#            data = map(ord, data[2:-2])
-#
-#            #Decode
-#            active =  bool(data[1] & 0x80)
-
         
 
 if __name__ == "__main__":
@@ -140,10 +129,12 @@ if __name__ == "__main__":
     #hw = PulserHardware(xem)
     hw.shutter = 0xfffffffe
 
-    hw.ppUpload( bytearray(b'\x03\x00\x00\x07\x00\x00\x00\x38\x00\x00\x00\x13\x8f\x00\x00\x00') )
+    hw.ppUpload( bytearray(b'\x06\x00\x00\x07\x00\x00\x00\x38\x07\x00\x00\x08\x00\x00\x00\x0f\x07\x00\x00\x0a\x00\x00\x00\x13\xff\xff\xff\x00\x00\x00\x00\x00') )
     hw.ppStart()
     for i in range(20):
-        hw.ppReadData(100,0.1)
+        data = hw.ppReadData(100,1.0)
+        print "read {0} bytes".format(len(data))
+        print data
     #print xem.ActivateTriggerIn(0x41, 0 )
 
     #hw.ppReadLog()
