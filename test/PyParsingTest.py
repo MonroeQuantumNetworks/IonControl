9f9fd9021dd993ecf12e6833a20e32174b2867e3
plmaunz <plmaunz@sandia.gov>
2013-02-15 16:02:57 -0700

    test

---
+++ test/PyParsingTest.py
@@ -0,0 +1,71 @@
+# fourFn.py
+#
+# Demonstration of the pyparsing module, implementing a simple 4-function expression parser,
+# with support for scientific notation, and symbols for e and pi.
+# Extended to add exponentiation and simple built-in functions.
+# Extended test cases, simplified pushFirst method.
+#
+# Copyright 2003-2006 by Paul McGuire
+#
+from pyparsing import Literal,CaselessLiteral,Word,Combine,Group,Optional,\
+    ZeroOrMore,Forward,nums,alphas
+import math
+import operator
+
+point = Literal( "." )
+e     = CaselessLiteral( "E" )
+fnumber = Combine( Word( "+-"+nums, nums ) + 
+                   Optional( point + Optional( Word( nums ) ) ) +
+                   Optional( e + Word( "+-"+nums, nums ) ) )
+ident = Word(alphas, alphas+nums+"_$")
+funit = Combine( fnumber + Optional(Word(" "," ")) + ident )
+ 
+plus  = Literal( "+" )
+minus = Literal( "-" )
+mult  = Literal( "*" )
+div   = Literal( "/" )
+lpar  = Literal( "(" ).suppress()
+rpar  = Literal( ")" ).suppress()
+addop  = plus | minus
+multop = mult | div
+expop = Literal( "^" )
+pi    = CaselessLiteral( "PI" )
+
+
+
+class Expression:
+    exprStack = []
+
+    def __init__(self, variabledict=dict()):
+        """
+        expop   :: '^'
+        multop  :: '*' | '/'
+        addop   :: '+' | '-'
+        integer :: ['+' | '-'] '0'..'9'+
+        atom    :: PI | E | real | fn '(' expr ')' | '(' expr ')'
+        factor  :: atom [ expop factor ]*
+        term    :: factor [ multop factor ]*
+        expr    :: term [ addop term ]*
+        """
+        expr = Forward()
+        atom = (Optional("-") + ( pi | e | funit | fnumber | ident + lpar + expr + rpar | ident ).setParseAction( self.pushFirst ) | ( lpar + expr.suppress() + rpar )).setParseAction(self.pushUMinus) 
+        
+        # by defining exponentiation as "atom [ ^ factor ]..." instead of "atom [ ^ atom ]...", we get right-to-left exponents, instead of left-to-righ
+        # that is, 2^3^2 = 2^(3^2), not (2^3)^2.
+        factor = Forward()
+        factor << atom + ZeroOrMore( ( expop + factor ).setParseAction( self.pushFirst ) )
+        
+        term = factor + ZeroOrMore( ( multop + factor ).setParseAction( self.pushFirst ) )
+        expr << term + ZeroOrMore( ( addop + term ).setParseAction( self.pushFirst ) )
+        self.bnf = expr
+        self.variabledict = variabledict
+        self.funit = fnumber + ZeroOrMore(ident)
+
+
+        
+
+if __name__ == "__main__":
+    
+    ExprEval = Expression()
+    
+ 
