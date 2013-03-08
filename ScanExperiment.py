8b525bbfb2037233c4840e388c0c1e598290f509
pmaunz <peter@maunz.name>
2013-03-07 23:05:10 -0700

    switch to scan Mode combo box

--- ScanExperiment.py
+++ ScanExperiment.py
@@ -112,7 +112,7 @@ class ScanExperiment(ScanExperimentForm, MainWindowWidget.MainWindowWidget):
         self.tracefilename, components = directory.sequencefile( self.scanSettings.filename )
         # get parameter to scan and scanrange
         self.scan = self.scanParametersWidget.getScan()
-        if self.scan.stepInPlace:
+        if self.scan.scanMode == self.scanParametersWidget.ScanModes.StepInPlace:
             self.scan.code = self.pulseProgramUi.pulseProgram.variableScanCode(self.scan.name, [self.scan.list[0]])
             mycode = self.pulseProgramUi.pulseProgram.variableScanCode(self.scan.name, [self.scan.list[0]]*2)
         else:
@@ -158,7 +158,7 @@ class ScanExperiment(ScanExperimentForm, MainWindowWidget.MainWindowWidget):
         """
         print "onData", len(data.count[self.scanSettings.counter]), data.scanvalue
         mean = numpy.mean( data.count[self.scanSettings.counter] )
-        if self.scan.stepInPlace:
+        if self.scan.scanMode == self.scanParametersWidget.ScanModes.StepInPlace:
             x = self.currentIndex
         else:
             x = self.scan.list[self.currentIndex].ounit(self.scan.start.out_unit).toval()
@@ -171,11 +171,11 @@ class ScanExperiment(ScanExperimentForm, MainWindowWidget.MainWindowWidget):
             self.currentTrace.filename = self.tracefilename
             print "Filename:" , self.tracefilename
             self.plottedTrace = Traceui.PlottedTrace(self.currentTrace,self.graphicsView,pens.penList)
-            if not self.scan.stepInPlace:
+            if not self.scan.scanMode == self.scanParametersWidget.ScanModes.StepInPlace:
                 self.graphicsView.setXRange( self.scan.start.toval(), self.scan.stop.ounit(self.scan.start.out_unit).toval() )
             self.traceui.addTrace(self.plottedTrace,pen=-1)
         else:
-            if self.scan.stepInPlace and len(self.currentTrace.x)>=self.scan.steps:
+            if self.scan.scanMode == self.scanParametersWidget.ScanModes.StepInPlace and len(self.currentTrace.x)>=self.scan.steps:
                 self.currentTrace.x = numpy.append(self.currentTrace.x[-self.scan.steps+1:], x)
                 self.currentTrace.y = numpy.append(self.currentTrace.y[-self.scan.steps+1:], mean)
             else:
@@ -189,12 +189,12 @@ class ScanExperiment(ScanExperimentForm, MainWindowWidget.MainWindowWidget):
         if data.final:
             self.currentTrace.header = self.pulseProgramUi.pulseProgram.currentVariablesText("#")
             self.currentTrace.resave()
-            if self.scan.repeat:
+            if self.scan.scanMode == self.scanParametersWidget.ScanModes.RepeatedScan:
                 self.onStart()
             else:
                 self.onStop()
         else:
-            if self.scan.stepInPlace:
+            if self.scan.scanMode == self.scanParametersWidget.ScanModes.StepInPlace:
                 self.pulserHardware.ppWriteData(self.scan.code)     
         self.scanParametersWidget.progressBar.setValue(self.currentIndex)
 
--- ScanParameters.py
+++ ScanParameters.py
@@ -9,6 +9,7 @@ import PyQt4.uic
 ScanExperimentForm, ScanExperimentBase = PyQt4.uic.loadUiType(r'ui\ScanParameters.ui')
 
 import ScanList
+from modules import enum
 
 class Scan:
     pass
@@ -19,11 +20,11 @@ class Settings:
     maximum = "10 ms"
     steps = 10
     scantype = 0
-    repeat = False
-    stepInPlace = False
+    scanMode = 0
     rewriteDDS = False
 
 class ScanParameters(ScanExperimentForm, ScanExperimentBase ):
+    ScanModes = enum.enum('SingleScan','RepeatedScan','StepInPlace')
     def __init__(self,config,parentname,parent=0):
         ScanExperimentForm.__init__(self,parent)
         ScanExperimentBase.__init__(self)
@@ -37,10 +38,9 @@ class ScanParameters(ScanExperimentForm, ScanExperimentBase ):
         self.maximumBox.setValue(self.settings.maximum)
         self.stepsBox.setValue(self.settings.steps)
         self.scanTypeCombo.setCurrentIndex(self.settings.scantype )
-        self.repeatCheckBox.setChecked( self.settings.repeat )
-        self.stepInPlaceCheckBox.setChecked( self.settings.stepInPlace )
         self.rewriteDDSCheckBox.setChecked( self.settings.rewriteDDS )
         self.progressBar.setVisible( False )
+        self.scanModeComboBox.setCurrentIndex( self.settings.scanMode )
 
     def setVariables(self, variabledict):
         self.variabledict = variabledict
@@ -62,12 +62,10 @@ class ScanParameters(ScanExperimentForm, ScanExperimentBase ):
         self.settings.maximum = self.maximumBox.value()
         self.settings.steps = self.stepsBox.value()
         self.settings.scantype = self.scanTypeCombo.currentIndex()
-        self.settings.repeat = self.repeatCheckBox.isChecked()
-        self.settings.stepInPlace = self.stepInPlaceCheckBox.isChecked()
         self.settings.rewriteDDS = self.rewriteDDSCheckBox.isChecked()
-        Scan.repeat = self.settings.repeat
-        Scan.stepInPlace = self.settings.stepInPlace
+        self.settings.scanMode = self.scanModeComboBox.currentIndex()
         Scan.rewriteDDS = self.settings.rewriteDDS
+        Scan.scanMode = self.settings.scanMode
         return Scan
         
     def onClose(self):
