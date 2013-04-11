807bd0d2ab9141f16b1ee07bba061b023e0df7f8
pmaunz <peter@maunz.name>
2013-04-10 22:03:34 -0600

    NewExternalScan

--- NewExternalScanExperiment.py
+++ NewExternalScanExperiment.py
@@ -1,31 +1,42 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Apr 10 21:36:17 2013
-
+Scan that varies a parameter outside the FPGA
+it uses ScanExperiment and only overwrites procedures that need to handle
+stuff differently.
 @author: pmaunz
 """
 
 import ScanExperiment 
 import time
+import numpy
+import Trace
+import functools
+import Traceui
+import pens
+import ExternalScannedParameters
 
 class NewExternalScanExperiment( ScanExperiment.ScanExperiment ):
+    def setupUi(self,MainWindow,config):
+        super(NewExternalScanExperiment,self).setupUi(MainWindow,config)
+        self.scanParametersWidget.setScanNames(ExternalScannedParameters.ExternalScannedParameters.keys())
+        
+    def setPulseProgramUi(self,pulseProgramUi):
+        self.pulseProgramUi = pulseProgramUi.addExperiment(self.experimentName)
+        
     def onStart(self):
         start = time.time()
         self.state = self.OpStates.running
         self.scanSettings = self.scanSettingsWidget.settings
-        # get parameter to scan and scanrange
         self.scan = self.scanParametersWidget.getScan()
-        if self.scan.scanMode == self.scanParametersWidget.ScanModes.StepInPlace:
-            self.scan.code = self.pulseProgramUi.pulseProgram.variableScanCode(self.scan.name, [self.scan.list[0]])
-            mycode = self.pulseProgramUi.pulseProgram.variableScanCode(self.scan.name, [self.scan.list[0]]*2)
-        else:
-            self.scan.code = self.pulseProgramUi.pulseProgram.variableScanCode(self.scan.name, self.scan.list)
-            mycode = self.scan.code
+        self.externalParameter = ExternalScannedParameters.ExternalScannedParameters[self.scan.name]()
+        self.externalParameter.saveValue()
+        self.externalParameterIndex = 0
+        self.externalParameter.setValue( self.scan.list[self.externalParameterIndex])
+                
         self.pulserHardware.ppFlushData()
         self.pulserHardware.ppClearWriteFifo()
         self.pulserHardware.ppUpload(self.pulseProgramUi.getPulseProgramBinary())
-        self.pulserHardware.ppWriteData(mycode)
-        print "Starting"
         self.pulserHardware.ppStart()
         self.running = True
         self.currentIndex = 0
@@ -34,32 +45,23 @@ class NewExternalScanExperiment( ScanExperiment.ScanExperiment ):
             if self.scan.autoSave:
                 self.currentTrace.resave()
             self.currentTrace = None
-        self.scanParametersWidget.progressBar.setRange(0,len(self.scan.list))
+        self.scanParametersWidget.progressBar.setRange(0,float(len(self.scan.list)))
         self.scanParametersWidget.progressBar.setValue(0)
         self.scanParametersWidget.progressBar.setVisible( True )
         self.timestampsNewRun = True
         print "elapsed time", time.time()-start
-    
-    def onStop(self):
-        if self.running:
-            self.pulserHardware.ppStop()
-            self.pulserHardware.ppClearWriteFifo()
-            self.pulserHardware.ppFlushData()
-            self.running = False
-            if self.scan.rewriteDDS:
-                self.NeedsDDSRewrite.emit()
-        self.scanParametersWidget.progressBar.setVisible( False )
-    
+
+
     def onData(self, data ):
         """ Called by worker with new data
         """
         print "onData", len(data.count[self.scanSettings.counter]), data.scanvalue
         mean, error = self.scanSettings.evalAlgo.evaluate( data.count[self.scanSettings.counter] )
-        #mean = numpy.mean( data.count[self.scanSettings.counter] )
         if self.scan.scanMode == self.scanParametersWidget.ScanModes.StepInPlace:
             x = self.currentIndex
         else:
-            x = self.scan.list[self.currentIndex].ounit(self.scan.start.out_unit).toval()
+            x = self.externalParameter.currentExternalValue() 
+        print "data", x, mean 
         if self.currentTrace is None:
             self.currentTrace = Trace.Trace()
             self.currentTrace.x = numpy.array([x])
@@ -79,13 +81,14 @@ class NewExternalScanExperiment( ScanExperiment.ScanExperiment ):
                 self.currentTrace.x = numpy.append(self.currentTrace.x, x)
                 self.currentTrace.y = numpy.append(self.currentTrace.y, mean)
             self.plottedTrace.replot()
-        self.currentIndex += 1
+        self.externalParameterIndex += 1
         self.showHistogram(data)
         if self.timestampSettingsWidget.settings.enable: 
             self.showTimestamps(data)
-        if data.final:
-            self.finalizeData()
+        if self.externalParameterIndex<len(self.scan.list):
+            self.externalParameter.setValue( self.scan.list[self.externalParameterIndex])
+            self.pulserHardware.ppStart()
+            print "External Value:" , self.scan.list[self.externalParameterIndex]
         else:
-            if self.scan.scanMode == self.scanParametersWidget.ScanModes.StepInPlace:
-                self.pulserHardware.ppWriteData(self.scan.code)     
-        self.scanParametersWidget.progressBar.setValue(self.currentIndex)
+            self.finalizeData()
+        self.scanParametersWidget.progressBar.setValue(float(self.externalParameterIndex))
