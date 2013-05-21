016c312aa16b62cbd11e02d205d1b47f732eee11
Peter Maunz <plmaunz@sandia.gov>
2013-05-21 09:10:57 -0600

    starting to add save raw data of timestamps; not done yet

--- TimestampSettings.py
+++ TimestampSettings.py
@@ -15,12 +15,14 @@ TimestampSettingsForm, TimestampSettingsBase = PyQt4.uic.loadUiType(r'ui\Timesta
 from modules import enum
 
 class Settings:
-    enable = False
-    binwidth =  magnitude.mg(1,'us')
-    roiStart =  magnitude.mg(0,'us')
-    roiWidth =  magnitude.mg(1,'ms')
-    integrate = 0
-    channel = 0
+    def __init__(self):
+        self.enable = False
+        self.binwidth =  magnitude.mg(1,'us')
+        self.roiStart =  magnitude.mg(0,'us')
+        self.roiWidth =  magnitude.mg(1,'ms')
+        self.integrate = 0
+        self.channel = 0
+        self.saveRawData = False
     
 class TimestampSettings(TimestampSettingsForm, TimestampSettingsBase ):
     integrationMode = enum.enum('IntegrateAll','IntegrateRun','NoIntegration')    
@@ -35,13 +37,15 @@ class TimestampSettings(TimestampSettingsForm, TimestampSettingsBase ):
     def setupUi(self, parent):
         TimestampSettingsForm.setupUi(self,parent)
         self.enableCheckBox.setChecked(self.settings.enable)
+        self.saveRawDataCheckBox.setChecked(self.settings.saveRawData)
         self.binwidthSpinBox.setValue(self.settings.binwidth)
         self.binwidthSpinBox.valueChanged.connect( functools.partial(self.onValueChanged, 'binwidth') )
         self.roiStartSpinBox.setValue(self.settings.roiStart)
         self.roiStartSpinBox.valueChanged.connect( functools.partial(self.onValueChanged, 'roiStart') )
         self.roiWidthSpinBox.setValue(self.settings.roiWidth)
         self.roiWidthSpinBox.valueChanged.connect( functools.partial(self.onValueChanged, 'roiWidth') )
-        self.enableCheckBox.stateChanged.connect( self.onStateChanged )
+        self.enableCheckBox.stateChanged.connect( functools.partial(self.onStateChanged,' enable' ) )
+        self.saveRawDataCheckBox.stateChanged.connect( functools.partial(self.onStateChanged,' saveRawData' ) )
         self.integrateCombo.setCurrentIndex( self.settings.integrate )
         self.integrateCombo.currentIndexChanged[int].connect( self.onIntegrationChanged )
         self.channelSpinBox.setValue( self.settings.channel )
@@ -51,8 +55,8 @@ class TimestampSettings(TimestampSettingsForm, TimestampSettingsBase ):
         print "ValueChanged", param, value
         setattr(self.settings, param, value)
         
-    def onStateChanged(self, state):
-        self.settings.enable = state == QtCore.Qt.Checked
+    def onStateChanged(self, attr, state):
+        setattr(self.settings,attr,state == QtCore.Qt.Checked)
         
     def onIntegrationChanged(self, value):
         self.settings.integrate = value
