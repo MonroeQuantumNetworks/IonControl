bba1c4ca1907669976d69beac8c01b1adc712716
Peter Maunz <plmaunz@sandia.gov>
2013-09-12 12:11:10 -0600

    Add script to generate sequence for randomized benchmarking

--- PrepAndAnalyze/CreateRandomizedBenchmarking.py
+++ PrepAndAnalyze/CreateRandomizedBenchmarking.py
@@ -3,19 +3,54 @@
 Created on Mon Sep 09 14:53:25 2013
 
 @author: plmaunz
+
+Generate the sequences necessary for randomized benchmarking
 """
 
 import xml.etree.ElementTree as ElementTree
 from xml.dom import minidom
 import random
+from collections import Counter
+import os.path
 
-Paulis = ['I', 'xp', 'yp', '-xp', '-yp' ]
-Cliffords = ['I', 'xc', 'yc', '-xc', '-yc' ]
-SequenceLength = range(0,101,10)
+Paulis = ['Ip', 'xp', 'Ip', 'yp', 'Ip', '-xp', 'Ip', '-yp' ]
+Cliffords = ['Ic', 'xc', 'yc', '-xc', '-yc' ]
+
+SequenceLengths = range(10,101,10)
 Randomizations = 100
-
 filename = "Randomized-1.xml"
-    
+path = ""
+
+class BlochState:
+    positions = ['d','u','i','-i','1','-1']  # the 6 points on the Bloch sphere
+    Transitions = { 'Ip':{'d':'d','u':'u','i':'i','-i':'-i','1':'1','-1':'-1'}, # Transitions between state as lookup tables
+                    'xp':{'d':'u','u':'d','i':'i','-i':'-i','1':'-1','-1':'1'},
+                    'yp':{'d':'u','u':'d','i':'-i','-i':'i','1':'1','-1':'-1'}, 
+                    '-xp':{'d':'u','u':'d','i':'i','-i':'-i','1':'-1','-1':'1'},
+                    '-yp':{'d':'u','u':'d','i':'-i','-i':'i','1':'1','-1':'-1'},
+                    'Ic':{'d':'d','u':'u','i':'i','-i':'-i','1':'1','-1':'-1'},
+                    'xc':{'d':'1','u':'-1','i':'i','-i':'-i','1':'u','-1':'d'},
+                    'yc':{'d':'i','u':'-i','i':'u','-i':'d','1':'1','-1':'-1'},
+                    '-xc':{'d':'-1','u':'1','i':'i','-i':'-i','1':'d','-1':'u'},
+                    '-yc':{'d':'-i','u':'i','i':'d','-i':'u','1':'1','-1':'-1'}}
+    TransitionToZLookup = { 'd':['Ic'], 'u':['Ic'], 'i':['yc','-yc'], '-i':['yc','-yc'], '1':['xc','-xc'], '-1':['xc','-xc']} # Lookup clifford gate that stransitions a state into the z-basis
+                    
+    def __init__(self):
+        self.state = 'd'
+        self.sequence = list()
+                           
+    def transition(self, gate):
+        """transition the state with the gate and keep track of the sequence of applied gates
+        """
+        self.state = self.Transitions[gate][self.state]
+        self.sequence.append(gate)
+        
+    def transitionToZ(self):
+        """ Add Clifford gate to transition into the z-Basis
+        """
+        gateList = self.TransitionToZLookup[self.state]
+        self.transition( gateList[random.randrange(0,len(gateList))] )
+        
 def prettify(elem):
     """Return a pretty-printed XML string for the Element.
     """
@@ -23,46 +58,45 @@ def prettify(elem):
     reparsed = minidom.parseString(rough_string)
     return reparsed.toprettyxml(indent="  ")
 
-def gateSequence(parent, name, sequence, index):
-    e  = ElementTree.SubElement(parent, 'GateSequence', {'name': name, 'index': str(index)})
+def gateSequence(parent, sequence, name, index, expected):
+    """Create the XML Element representing one gate sequence from that sequence.
+        The element is given the supplied name, index and expected attributes
+    """
+    e  = ElementTree.SubElement(parent, 'GateSequence', {'name': name, 'index': str(index), 'expected':expected})
     e.text = ", ".join(sequence)
-
-root = ElementTree.Element('GateSetDefinition')
-
-
+    return e
 
 
 def createRandomization(length):
-    randomization = list()
+    """create one randomization given length
+       returns the BlochState object representing the sequence
+    """
+    state = BlochState()
     for i in range(length):
-        randomization.append( Paulis[random.randrange(0,len(Paulis))])
-        
+        state.transition( Paulis[random.randrange(0,len(Paulis))] )
+        state.transition( Cliffords[random.randrange(0,len(Cliffords))] )       
+    state.transition( Paulis[random.randrange(0,len(Paulis))] )
+    state.transitionToZ()
+    state.transition( Paulis[random.randrange(0,len(Paulis))] )
+    return state
+    
+if __name__=="__main__":    
 
+    root = ElementTree.Element('GateSetDefinition')
+    index = 0     # keep track of the current index
+    outcomes = list() # list of all expected outcomes  
+    totallength = 0   # total number of gates
+    for length in SequenceLengths:
+        for randindex in range(Randomizations): 
+            state = createRandomization(length)
+            gateSequence( root, state.sequence, "{0},{1}".format(length,randindex), index, state.state)
+            outcomes.append(state.state)
+            totallength += len(state.sequence)
+            index += 1
 
-
-
-i = 0
-root.append( ElementTree.Comment('Strings of length 0'))
-gateSequence(root,str(i),[],i)
-i += 1
-for length in range(1,4):
-    root.append( ElementTree.Comment('Strings of length {0}'.format(length)))
-    for sequence in allstrings(basis,length):
-        gateSequence(root,str(i),sequence,i)
-        i += 1
-
-
-root.append( ElementTree.Comment('Inverted Strings of length {0}'.format(0)))
-gateSequence(root,str(i),['x2'],i)
-i += 1
-for length in range(1,4):
-    root.append( ElementTree.Comment('Inverted Strings of length {0}'.format(length)))
-    for sequence in allstrings(basis,length):
-        gateSequence(root,str(i),sequence+['x2'],i)
-        i += 1
-
-
-with open(filename,'w') as f:
-    f.write(prettify(root))
-
-print i
+    with open(os.path.join(path,filename),'w') as f:
+        f.write(prettify(root))
+    
+    print index
+    print Counter(outcomes)
+    print totallength
