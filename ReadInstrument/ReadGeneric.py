dc0fadf173e12516628912e1ebce20bd532305b5
pmaunz <peter@maunz.name>
2013-09-14 17:35:02 -0600

    Added ReadInstrument code. Still has to be adapted to the newer library modules and the iimports have to be resolved

---
+++ ReadInstrument/ReadGeneric.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 19 10:24:45 2012
+
+@author: plmaunz
+"""
+
+from .. import Trace
+
+class ReadGeneric:
+    def readTrace(self):
+        return Trace.Trace()
---
+++ ReadInstrument/Read_E5100B.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 14 15:37:21 2012
+
+@author: plmaunz
+"""
+
+import visa
+import numpy
+import ReadGeneric
+
+from .. import Trace
+
+class E5100B(ReadGeneric.ReadGeneric):
+    def __init__(self,address):
+        self.GPIB = visa.instrument(address)
+        
+    def readTrace(self):
+        self.t = Trace.Trace()
+        self.t.addColumn('real')
+        self.t.addColumn('imaginary')
+        self.t.addColumn('amplitude')       
+        self.t.vars.Instrument = self.GPIB.ask("*IDN?")
+        self.t.vars.Center = float(self.GPIB.ask("CENT?"))
+        self.t.vars.Start = float(self.GPIB.ask("STAR?") )
+        self.t.vars.Stop = float(self.GPIB.ask("STOP?"))
+        self.t.rawTrace = numpy.array(self.GPIB.ask("OUTPDATA?").split(","),dtype=float)
+        self.t.Trace = self.t.rawTrace.view(complex)
+        self.t.vars.Step = (self.t.vars.Stop-self.t.vars.Start)/(self.t.Trace.size-1)
+        self.t.x = numpy.arange(self.t.vars.Start,self.t.vars.Stop+self.t.vars.Step,self.t.vars.Step)
+        self.t.y = 10*numpy.log(numpy.abs(self.t.Trace))
+        self.t.real = numpy.real(self.Trace)
+        self.t.imaginary = numpy.imag(self.Trace)
+        self.t.amplitude = numpy.imag(self.Trace)
+        return self.t
+        
+if __name__== "__main__":
+    Inst = E5100B("GPIB::17")
+    t = Inst.readTrace()
+    print t.vars.__dict__
+    print numpy.abs(t.Trace)
+    print t.Trace.size
+    print t.TraceX.size
+    Inst.save("Resonator_4.txt")
+    pyplot.plot( t.TraceX, 10*numpy.log(numpy.abs(t.Trace)) )
+    pyplot.show()
+ 
