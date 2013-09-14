cc2f1d3280ce01c353739d99a88431f0249551b3
pmaunz <peter@maunz.name>
2013-09-14 17:34:14 -0600

    tries in Magnitude utilit. New stuff does not work, but does not harm either

--- modules/MagnitudeUtilit.py
+++ modules/MagnitudeUtilit.py
@@ -6,6 +6,7 @@ Created on Tue Jul 02 14:25:44 2013
 """
 
 import magnitude
+import math
 
 def value( obj, tounit=None ):
     """ return the value of a magnitude object, or float"""
@@ -32,3 +33,33 @@ def mg( value, unit=None ):
     if unit:
         return magnitude.mg(value,unit)
     return magnitude.mg(value)
+    
+_reverse_prefix = { -24: 'y',  # yocto
+                    -21: 'z',  # zepto
+                    -18: 'a',  # atto
+                    -15: 'f',  # femto
+                    -12: 'p',  # pico
+                    -9: 'n',   # nano
+                    -6: 'u',   # micro
+                    -3: 'm',   # mili
+                    -2: 'c',   # centi
+                    -1: 'd',   # deci
+                    3: 'k',    # kilo
+                    6: 'M',    # mega
+                    9: 'G',    # giga
+                    12: 'T',   # tera
+                    15: 'P',   # peta
+                    18: 'E',   # exa
+                    21: 'Z',   # zetta
+                    24: 'Y'   # yotta
+                    }
+                    
+#def reprefix(mag):
+#    """change the prefix of the output unit of a magnitude
+#    such that the value is between 1 and 1000
+#    """
+#    if mag.val == 0:
+#        return mag
+#    order = 3*int( math.floor( math.log10( abs(mag.val) )/3 ) )
+    
+    
