bc2989e40334614b49014aa02cbb456aa70f8462
Peter Maunz <plmaunz@sandia.gov>
2013-09-18 17:12:36 -0600

    have coordinateplotwidget show the coordinated with adapting precision

--- CoordinatePlotWidget.py
+++ CoordinatePlotWidget.py
@@ -7,6 +7,8 @@ Created on Wed Jan 09 21:19:26 2013
 
 import pyqtgraph
 from PyQt4 import QtGui, QtCore
+import math
+from modules.round import roundToNDigits
 
 class CoordinatePlotWidget(pyqtgraph.GraphicsLayoutWidget):
 
@@ -16,14 +18,19 @@ class CoordinatePlotWidget(pyqtgraph.GraphicsLayoutWidget):
         self.graphicsView = self.addPlot(row=0,col=0)
         self.addItem(self.label,row=1,col=0)
         self.graphicsView.scene().sigMouseMoved.connect(self.onMouseMoved)
-        self.template = "<span style='font-size: 10pt'>x={0:.4f}, <span style='color: red'>y={1:.4f}</span></span>"
+        self.template = "<span style='font-size: 10pt'>x={0}, <span style='color: red'>y={1}</span></span>"
         self.mousePoint = None
         self.mousePointList = list()
     
     def onMouseMoved(self,pos):
         if self.graphicsView.sceneBoundingRect().contains(pos):
             self.mousePoint = self.graphicsView.vb.mapSceneToView(pos)
-            self.label.setText( self.template.format( self.mousePoint.x(), self.mousePoint.y() ) )
+            vR = self.graphicsView.vb.viewRange()
+            deltaX, deltaY = vR[0][1]-vR[0][0], vR[1][1]-vR[1][0]
+            precx = int( math.ceil( math.log10(abs(self.mousePoint.x()/deltaX)) ) + 3 )
+            precy = int( math.ceil( math.log10(abs(self.mousePoint.y()/deltaY)) ) + 3 )
+            roundedx, roundedy = roundToNDigits( self.mousePoint.x(),precx), roundToNDigits(self.mousePoint.y(), precy )
+            self.label.setText( self.template.format( repr(roundedx), repr(roundedy) ))
             
     def onCopyLocation(self,which):
         text = {'x': ("{0}".format(self.mousePoint.x())),
