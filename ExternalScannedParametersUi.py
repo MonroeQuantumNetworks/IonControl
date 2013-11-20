987f3f230d93b42302c6a3b7fb711f28b1b296a2
pmaunz <peter@maunz.name>
2013-11-19 21:16:21 -0700

    move MagnitudeSpinBoxDelegate into its own file

--- ExternalScannedParametersUi.py
+++ ExternalScannedParametersUi.py
@@ -8,43 +8,9 @@ Created on Fri Apr 12 23:45:54 2013
 import PyQt4.uic
 from PyQt4 import QtGui, QtCore
 import functools
+from MagnitudeSpinBoxDelegate import MagnitudeSpinBoxDelegate
 
 UiForm, UiBase = PyQt4.uic.loadUiType(r'ui\ExternalScannedParameterUi.ui')
-
-from MagnitudeSpinBox import MagnitudeSpinBox
-
-class MagnitudeSpinBoxDelegate(QtGui.QItemDelegate):
-  
-    """
-    This class is responsible for the combo box editor in the trace table,
-    which is used to select which pen to use in drawing the trace on the plot.
-    The pen icons are in the array penicons, which is determined when the delegate
-    is constructed.
-    """    
-    
-    def __init__(self):
-        """Construct the TraceComboDelegate object, and set the penicons array."""
-        QtGui.QItemDelegate.__init__(self)
-        
-    def createEditor(self, parent, option, index ):
-        """Create the combo box editor used to select which pen icon to use.
-           The for loop adds each pen icon into the combo box."""
-        editor = MagnitudeSpinBox(parent)
-        editor.valueChanged.connect( functools.partial( index.model().setValue, index.row() ))
-        return editor
-        
-    def setEditorData(self, editor, index):
-        print "setEditorData"
-        value = index.model().data(index, QtCore.Qt.EditRole) 
-        editor.setValue(value)
-        
-    def setModelData(self, editor, model, index):
-        value = editor.value()
-        model.setData(index, value, QtCore.Qt.EditRole)
-         
-    def updateEditorGeometry(self, editor, option, index ):
-        editor.setGeometry(option.rect)
-
 
 class ExternalParameterControlTableModel( QtCore.QAbstractTableModel ):
     valueChanged = QtCore.pyqtSignal(str, object)
