2f29fc28fad382e6e384f028cf31dfd6eeb6ed6a
Peter Maunz <plmaunz@sandia.gov>
2014-02-17 12:55:27 -0700

    add ReadInstrument files to index

---
+++ ReadInstrument/ReadGeneric.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 19 10:24:45 2012
+
+@author: plmaunz
+"""
+
+from trace import Trace
+
+
+class ReadGeneric:
+    def readTrace(self):
+        return Trace.Trace()
