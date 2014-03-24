cd15d6e41ce36a71c264e00c1080e9f77e4f6d16
Peter Maunz <plmaunz@sandia.gov>
2014-03-24 17:48:33 -0600

    add more detail in status messages of Opal Kelly boards

--- pulser/PulserHardwareServer.py
+++ pulser/PulserHardwareServer.py
@@ -614,7 +614,7 @@ class PulserHardwareServer(Process):
         if self.xem is not None and self.xem.IsOpen():
             check( self.xem.ConfigureFPGA(bitfile), "Configure bitfile {0}".format(bitfile))
             self.xem.ActivateTriggerIn(0x41, 9)  # reset overrun
-            logger.info("upload bitfile '{0}'".format(bitfile))
+            logger.info("upload bitfile '{0}' to {1}".format(bitfile,self.xem.GetSerialNumber()))
             logger.info(str(BitfileInfo(bitfile)))
 
     def openByName(self,name):
