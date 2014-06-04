c893615d165662d980f8d9cef98d748050ab3bb5
Peter Maunz <plmaunz@sandia.gov>
2014-06-04 08:21:05 -0600

    improve simplelogger to adapt frequency

--- Logging/SimpleLogger.py
+++ Logging/SimpleLogger.py
@@ -8,19 +8,30 @@ import time
 from MKSReader import MKSReader
 from TerranovaReader import TerranovaReader
 import os
+from modules.RunningStat import RunningStatHistogram
+
+MaxRecordingInterval = 120
+QueryInterval = 0.2
 
 if __name__=="__main__":
     mks = TerranovaReader() #MKSReader()
     mks.open()
+    HistogramStat = RunningStatHistogram()
+    LastRecordingTime = 0
     with open("pressurelog-ion.txt",'a') as f:
         while (True):
             try:
                 value = mks.value()
-                f.write("{0} {1}\n".format(datetime.datetime.now(), value))
-                print "{0} {1}".format(datetime.datetime.now(), value)
-                f.flush()
-                os.fsync(f)
-                time.sleep(10)
+                HistogramStat.add( value )
+                if time.time()-LastRecordingTime > MaxRecordingInterval or len(HistogramStat.histogram)>2:
+                    LastRecordingTime = time.time()
+                    message = "{0} {1} {2} {3} {4}".format(datetime.datetime.now(), HistogramStat.mean, HistogramStat.count, HistogramStat.min, HistogramStat.max )
+                    f.write(message + "\n")
+                    print message
+                    f.flush()
+                    os.fsync(f)
+                    HistogramStat.clear()
+                time.sleep(QueryInterval)
             except Exception as e:
                 print e
                 mks.close()
--- modules/RunningStat.py
+++ modules/RunningStat.py
@@ -5,7 +5,7 @@ Created on Sat Sep 14 07:25:59 2013
 @author: pmaunz
 """
 import math
-
+from collections import defaultdict
 
 # see Donald Knuth's Art of Computer Programming, Vol 2, page 232, 3rd edition
 class RunningStat(object):
@@ -18,6 +18,8 @@ class RunningStat(object):
         self.mNew = self.zero
         self.sOld = self.zero
         self.sNew = self.zero
+        self._max = None
+        self._min = None
         self.count = 0
         
     @property
@@ -35,6 +37,14 @@ class RunningStat(object):
     @property
     def stderr(self):
         return self.stddev / math.sqrt(self.count-1) if self.count>1 else 0
+    
+    @property
+    def min(self):
+        return self._min
+    
+    @property
+    def max(self):
+        return self._max
         
     def add(self, value):
         if not( value is None or math.isnan(value) or math.isinf(value) ):
@@ -44,10 +54,41 @@ class RunningStat(object):
                 self.mNew = value
                 self.sOld = 0
                 self.sNew = 0
+                self._max = value
+                self._min = value
             else:
                 self.mNew = self.mOld + ( value - self.mOld )/self.count
                 self.sNew = self.sOld + ( value - self.mOld ) * ( value - self.mNew )
+                self._max = max( self._max, value )
+                self._min = min( self._min, value )
                 
                 self.mOld = self.mNew
                 self.sOld = self.sNew
         
+        
+class RunningStatHistogram( RunningStat, object ):
+    def __init__(self, zero=0):
+        RunningStat.__init__(self,zero)
+        self._hist = defaultdict( lambda: 0)
+        
+    def add(self, value):
+        self._hist[value] += 1
+        RunningStat.add(self,value) 
+        
+    @property
+    def histogram(self):
+        return self._hist
+    
+    def clear(self):
+        RunningStat.clear(self)
+        self._hist = defaultdict( lambda: 0)
+         
+if __name__=="__main__":
+    RSH = RunningStatHistogram()
+    RSH.add(1.0)
+    RSH.add(2.0)
+    print RSH.mean 
+    RSH.clear()       
+    RSH.add(1.0)
+    RSH.add(2.0)
+    print RSH.mean 
