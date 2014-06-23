0418bb9d75d754081f64bf4374c78e0ccab58f0d
unknown <wolverine@R443863.son.sandia.gov>
2014-06-23 09:37:21 -0600

    bugfix in MKSReader and log two values in simple logger

--- Logging/SimpleLogger.py
+++ Logging/SimpleLogger.py
@@ -10,27 +10,34 @@ from TerranovaReader import TerranovaReader
 import os
 from modules.RunningStat import RunningStatHistogram
 
-MaxRecordingInterval = 120
+MaxRecordingInterval = 180
 QueryInterval = 0
 
 if __name__=="__main__":
-    mks = TerranovaReader() #MKSReader()
+    terra = TerranovaReader(port=3, timeout=0.5) #
+    terra.open()
+    mks = MKSReader(timeout=0.5)
     mks.open()
     HistogramStat = RunningStatHistogram()
+    TerraStat = RunningStatHistogram()
     LastRecordingTime = 0
-    with open("pressurelog-ion.txt",'a') as f:
+    with open("pressurelog-triangle.txt",'a') as f:
         while (True):
             try:
                 value = mks.value()
                 HistogramStat.add( value )
-                if time.time()-LastRecordingTime > MaxRecordingInterval or len(HistogramStat.histogram)>2:
+                value_terra = terra.value()
+                TerraStat.add( value_terra )
+                if time.time()-LastRecordingTime > MaxRecordingInterval or len(HistogramStat.histogram)>2 or len(TerraStat.histogram)>2:
                     LastRecordingTime = time.time()
-                    message = "{0} {1} {2} {3} {4}".format(datetime.datetime.now(), HistogramStat.mean, HistogramStat.count, HistogramStat.min, HistogramStat.max )
+                    message = "{0} {1} {2} {3} {4} {5} {6} {7} {8}".format(datetime.datetime.now(), HistogramStat.mean, HistogramStat.count, HistogramStat.min, HistogramStat.max,
+                                                                       TerraStat.mean, TerraStat.count, TerraStat.min, TerraStat.max )
                     f.write(message + "\n")
                     print message
                     f.flush()
                     os.fsync(f)
                     HistogramStat.clear()
+                    TerraStat.clear()
                 time.sleep(QueryInterval)
             except Exception as e:
                 print e
