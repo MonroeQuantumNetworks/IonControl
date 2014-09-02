b6267250ee0ed67e935cce399bb353388cff097c
wolverine <plmaunz@sandia.gov>
2014-09-02 12:26:43 -0600

    add freqfine option to set_dds

--- pppCompiler/Builtins.py
+++ pppCompiler/Builtins.py
@@ -80,6 +80,9 @@ def set_dds( symboltable, arg=list(), kwarg=dict()):
     if 'amp' in kwarg:
         freq = symboltable.getVar( kwarg['amp'] )
         commandlist.append( "  DDSAMP {0}, {1}".format(channel.name, freq.name))
+    if 'freqfine' in kwarg:
+        freq = symboltable.getVar( kwarg['freqfine'] )
+        commandlist.append( "  DDSFRQFINE {0}, {1}".format(channel.name, freq.name))        
     return commandlist
   
 def read_pipe( symboltable, arg=list(), kwarg=dict()):
