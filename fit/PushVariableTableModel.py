fcd8d79c1b073c90b592c10c9c0fc0c7d7fe0f89
pmaunz <peter@maunz.name>
2014-09-11 22:19:09 -0600

    fix wired crosstalk bug in fit

--- fit/PushVariableTableModel.py
+++ fit/PushVariableTableModel.py
@@ -117,7 +117,8 @@ class PushVariableTableModel(QtCore.QAbstractTableModel):
         return self.setDataLookup[(role,index.column())](index.row(),value)
     
     def setValue(self, index, value):
-        self.fitfunction.startParameters[index.row()] = value
+        #self.fitfunction.startParameters[index.row()] = value
+        self.setData( index, value, QtCore.Qt.EditRole)
 
     def flags(self, index ):
         if index.column()==0:
