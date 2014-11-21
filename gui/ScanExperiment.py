2cf71186fcc8a4b7a82fbc714352c32b54bc6477
pmaunz <peter@maunz.name>
2014-11-20 23:26:39 -0700

    add analysis reload button to fitui

--- gui/ScanExperiment.py
+++ gui/ScanExperiment.py
@@ -897,6 +897,7 @@ class ScanExperiment(ScanExperimentForm, MainWindowWidget.MainWindowWidget):
             pageWidth = printer.pageRect().width()
             pageHeight = printer.pageRect().height()
             exporter.parameters()['width'] = pageWidth*preferences.printWidth   # (note this also affects height parameter)
+            exporter.widthChanged()
               
             # save to file
             png = exporter.export(toBytes=True)
