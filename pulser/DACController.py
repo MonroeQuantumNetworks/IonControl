5ece5010f49cccfa448a2c8ded3413f0229c554c
wolverine <plmaunz@sandia.gov>
2014-12-22 12:02:44 -0700

    bugfixes in DAC code

--- pulser/DACController.py
+++ pulser/DACController.py
@@ -25,12 +25,15 @@ class DACController( OKBase ):
         if len(line)<self.channelCount:
             raise DACControllerException("Line contains only {0} voltages, need {1}".format(len(line), self.channelCount))
         startaddress = address * 2 * self.channelCount   # 2 bytes per channel, 96 channels
-        self.xem.WriteToPipeIn( 0x84, bytearray( struct.pack('=HQ', 0x1, startaddress)))  # write start address to extended wire 2
+        self.xem.WriteToPipeIn( 0x84, bytearray( struct.pack('=HQ', 0x3, startaddress)))  # write start address to extended wire 2
+        
         data = bytearray(numpy.array( self.toInteger(line), dtype=numpy.int16).view(dtype=numpy.int8))
+        print hex(data[0]), hex(data[1]), hex(data[2]), hex(data[3])
         return self.xem.WriteToPipeIn( 0x83, data )        
     
     def shuttle(self, startLine, beyondEndLine, idleCount=0, direction=0 ):
-        self.xem.WriteToPipeIn( 0x84, bytearray( struct.pack('=HQ', 0x1, (startLine * 2 * self.channelCount)<<32 + (beyondEndLine * 2 * self.channelCount))) ) # write start address to extended wire 2
+        self.xem.WriteToPipeIn( 0x84, bytearray( struct.pack('=HQHQ', 0x1, startLine*2*self.channelCount, 0x2, beyondEndLine*2*self.channelCount) )) # write start address to extended wire 2
+        check( self.xem.ActivateTriggerIn( 0x43, 6), 'ActivateTrigger' )
         self.xem.SetWireInValue( 0x01, idleCount & 0xffff )
         self.xem.SetWireInValue( 0x02, direction & 0x1 )
         self.xem.UpdateWireIns()
