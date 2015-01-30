e3d61287ffc200661f6b26b58f3051c838547fcc
wolverine <plmaunz@sandia.gov>
2015-01-29 17:06:34 -0700

    add CambrideInstruments code

---
+++ packages/CICW_RevC/mw2Lib/mw2Error.h
@@ -0,0 +1,38 @@
+﻿// Error codes were borrowed from CVI errno.h
+//
+#define	MW2ECNOERR	(INT32)0		/* No Error */
+#define	MW2ECPERM	(INT32)1	
+#define	MW2ECNOENT	(INT32)2		/* No such file or directory */
+#define	MW2ECSRCH	(INT32)3	
+#define	MW2ECINTR	(INT32)4	
+#define	MW2ECIO		(INT32)5		/* I/O error */
+#define	MW2ECNXIO	(INT32)6	
+#define	MW2EC2BIG	(INT32)7	
+#define	MW2ECNOEXEC	(INT32)8	
+#define	MW2ECBADF	(INT32)9		/* Bad file handle */
+#define	MW2ECCHILD	(INT32)10	
+#define	MW2ECAGAIN	(INT32)11	
+#define	MW2ECNOMEM	(INT32)12		/* Insufficient memory */
+#define	MW2ECACCES	(INT32)13		/* Access denied */
+#define	MW2ECFAULT	(INT32)14	
+#define	MW2ECNOTBLK	(INT32)15	
+#define	MW2ECBUSY	(INT32)16	
+#define	MW2ECEXIST	(INT32)17		/* File exists */
+#define	MW2ECXDEV	(INT32)18	
+#define	MW2ECNODEV	(INT32)19	
+#define	MW2ECNOTDIR	(INT32)20	
+#define	MW2ECISDIR	(INT32)21	
+#define	MW2ECINVAL	(INT32)22		/* Invalid argument */
+#define	MW2ECNFILE	(INT32)23	
+#define	MW2ECMFILE	(INT32)24		/* Too many open files */
+#define	MW2ECNOTTY	(INT32)25	
+#define	MW2ECTXTBSY	(INT32)26	
+#define	MW2ECFBIG	(INT32)27	
+#define	MW2ECNOSPC	(INT32)28		/* No space left on device */
+#define	MW2ECSPIPE	(INT32)29	
+#define	MW2ECROFS	(INT32)30	
+#define	MW2ECMLINK	(INT32)31	
+#define	MW2ECPIPE	(INT32)32	
+#define	MW2ECDOM	(INT32)33		/* domain error */
+#define	MW2ECRANGE	(INT32)34		/* range error */
+#define	MW2ECUNSPECIFIED	(INT32)−2147467259	/* Un-Specified Error 0x80004005 */ 
