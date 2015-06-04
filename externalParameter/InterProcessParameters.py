b18217d99f8292e6dd5718fd411b71570df4ae54
Jonathan Mizrahi <jmizrahi@umd.edu>
2015-06-04 14:43:35 -0400

    Add HP6632 control

--- externalParameter/InterProcessParameters.py
+++ externalParameter/InterProcessParameters.py
@@ -4,9 +4,6 @@ from ExternalParameterBase import ExternalParameterBase
 from multiprocessing.connection import Client
 
 class LockOutputFrequency(ExternalParameterBase):
-    """
-    Adjust the current on the N6700B current supply
-    """
     className = "Digital Lock Output Frequency"
     _dimension = magnitude.mg(200,'MHz')
     _outputChannels = { "OutputFrequency": "MHz" }    # a single channel with key None designates a device only supporting a single channel
