e43a37b2d37fd8e8954f8afb6384f4a01e74a39b
Jonathan Mizrahi <jmizrahi@umd.edu>
2015-07-30 15:44:43 -0400

    Add console, disable GUI elements while running

--- scripting/Script.py
+++ scripting/Script.py
@@ -9,39 +9,43 @@ import modules.magnitude as magnitude
 from PyQt4.QtCore import pyqtSignal, QThread, pyqtSlot, QEventLoop
 import inspect
 import traceback
+import time
+
+def longWait(): #DELETE THIS FUNCTION
+    time.sleep(1)
 
 class ScriptException(Exception):
     pass
 
-def scriptingFunction(func):
+def scriptFunction(func):
     """Mark a function as a scripting function"""
-    func.isScriptingFunction = True
+    func.isScriptFunction = True
     return func
 
 def checkScripting(func):
     """Check whether a function has been marked"""
-    return hasattr(func, 'isScriptingFunction')
+    return hasattr(func, 'isScriptFunction')
 
 class Script(QThread):
     """Encapsulates a running script together with all the scripting functions. Script executes in separate thread."""
     locationSignal = pyqtSignal(int)
+    consoleSignal = pyqtSignal(str, bool)
     completed = pyqtSignal()
     def __init__(self, globalVariablesUi, fullname='', code='', parent=None):
         super(Script,self).__init__(parent)
         self.fullname = fullname #Full name, with path
         self.shortname = os.path.basename(fullname)
         self.code = code #The code in the script
-        self.scriptingFunctionNames = []
+        self.scriptFunctions = []
         self.globalVariablesUi = globalVariablesUi
-        
+
     def run(self):
         """run the script"""
         #Name local functions with the same names as the class functions, so that the script can call them by name directly
-        for name in self.scriptingFunctionNames:
+        for name in self.scriptFunctions:
             locals()[name] = getattr(self, name)
         execfile(self.fullname) #run the script
         self.completed.emit()
-        
         
     def emitLocation(self):
         """Emits a signal containing the current script location"""
@@ -52,55 +56,69 @@ class Script(QThread):
         locs = [loc for loc in stack_trace if loc[0] == self.fullname] #Find the locations that match the script name
         scriptLoc = locs[0] if locs != [] else (None, -1, None, None)
         if scriptLoc[1] >= 0:
-            logger.info("Executing {0} in {1} at line {2}".format( scriptLoc[3], os.path.basename(scriptLoc[0]), scriptLoc[1] ))
+            message = "Executing {0} in {1} at line {2}".format( scriptLoc[3], os.path.basename(scriptLoc[0]), scriptLoc[1] )
+            logger.debug(message)
+            self.consoleSignal.emit(message, True)
         else:
-            logger.warning("Not executing script")
-        self.locationSignal.emit(scriptLoc[1]) #Emit a signal containing the script location 
+            message = "Not executing script"
+            logger.warning(message)
+            self.consoleSignal.emit(message, False)
+        self.locationSignal.emit(scriptLoc[1]) #Emit a signal containing the script location
         
-    @scriptingFunction
+    @scriptFunction
     def startScan(self):
         """Start the scan. This is equivalent to clicking the "start" button on the GUI."""
         pass
     
-    @scriptingFunction
-    def setGlobal(self, name, value, unit, create=False):
-        """set the global "name" to "value" with the given unit. This is equivalent to typing in a value+unit in the globals GUI.
-        If create=True, global will be created if name does not exist.
-        If create=False, ScriptingException will be raised if name does not exist."""
+    @scriptFunction
+    def setGlobal(self, name, value, unit):
+        """set the global "name" to "value" with the given unit. This is equivalent to typing in a value+unit in the globals GUI."""
         logger = logging.getLogger(__name__)
         self.emitLocation()
+        longWait()
         doesNotExist = (name not in self.globalVariablesUi.keys())
-        import time #DELETE THIS
-        time.sleep(1) #DELETE THIS
-        if not create and doesNotExist:
-            errorMessage = "Global variable {0} does not exist.".format(name)
-            logger.error(errorMessage)
-            raise ScriptException(errorMessage)
-        elif doesNotExist:
-            self.globalVariablesUi.model.addVariable(name)
-            logger.info("Global variable {0} created".format(name))
+        if doesNotExist:
+            message = "Global variable {0} does not exist.".format(name)
+            logger.error(message)
+            self.consoleSignal.emit(message, False)
+            raise ScriptException(message)
         self.globalVariablesUi.update([('Global', name, magnitude.mg(value,unit))])
-        logger.info("Global variable {0} set to {1} {2}".format(name, value, unit))
+        message = "Global variable {0} set to {1} {2}".format(name, value, unit)
+        self.consoleSignal.emit(message, True)
+        logger.info(message)
+        
+    @scriptFunction
+    def addGlobal(self, name, value, unit):
+        """add the global "name" to the list of globals, and set its value to "value" with the given unit."""
+        logger = logging.getLogger(__name__)
+        self.emitLocation()
+        doesNotExist = name not in self.globalVariablesUi.keys()
+        if doesNotExist:
+            self.globalVariablesUi.model.addVariable(name)
+            message = "Global variable {0} created".format(name)
+            logger.info(message)
+            self.consoleSignal.emit(message, True)
+        self.setGlobal(name, value, unit)
 
-    @scriptingFunction
+
+    @scriptFunction
     def setScan(self, name):
         """set the scan interface to "name." This is equivalent to selecting "name" from the scan dropdown menu."""
         pass
     
-    @scriptingFunction
+    @scriptFunction
     def setEvaluation(self, name):
         """set the evaluation interface to "name." This is equivalent to selecting "name" from the evaluation dropdown menu."""
         pass
     
-    @scriptingFunction
+    @scriptFunction
     def setAnalysis(self, name):
         """set the analysis interface to "name." This is equivalent to selecting "name" from the analysis dropdown."""
         pass
     
-    @scriptingFunction
+    @scriptFunction
     def pause(self):
         """Pause the script."""
     
-scriptingFunctionNames = [a[0] for a in inspect.getmembers(Script, checkScripting)] #Get the names of the scripting functions
-scriptingFunctionDocs = [getattr(Script, name).__doc__ for name in scriptingFunctionNames] #docstrings of the scripting functions
-scriptingFunctions = zip(scriptingFunctionNames, scriptingFunctionDocs) #dict of the form name:docstring, for each scripting function
+scriptFunctions = [a[0] for a in inspect.getmembers(Script, checkScripting)] #Get the names of the scripting functions
+scriptFunctionDocs = dict(zip(scriptFunctions,[getattr(Script, name).__doc__ for name in scriptFunctions])) #docstrings of the scripting functions
--- scripting/ScriptingUi.py
+++ scripting/ScriptingUi.py
@@ -7,15 +7,16 @@ Created on Jul 24, 2015
 import os.path
 
 #from PyQt4 import QtCore, QtGui
-from PyQt4.QtGui import QFileDialog, QDialog, QIcon, QColor
-from PyQt4.QtCore import pyqtSlot
+from PyQt4 import QtGui
+from PyQt4 import QtCore
 import PyQt4.uic
 import logging
 from PyQt4.Qsci import QsciScintilla
 
 from gui import ProjectSelection
 from modules.PyqtUtility import BlockSignals
-from Script import Script, ScriptException, scriptingFunctionNames
+from Script import Script, ScriptException, scriptFunctions
+from pulseProgram.PulseProgramSourceEdit import PulseProgramSourceEdit
 
 ScriptingWidget, ScriptingBase = PyQt4.uic.loadUiType('ui/Scripting.ui')
 
@@ -25,6 +26,7 @@ class ScriptingUi(ScriptingWidget,ScriptingBase):
         ScriptingBase.__init__(self)
         self.config = config
         self.textEdit = None
+        self.console = None
         self.recentFiles = dict() #dict of form {shortname: fullname}, where fullname has path and shortname doesn't
         self.globalVariablesUi = globalVariablesUi 
         self.script = Script(self.globalVariablesUi) #encapsulates the script
@@ -38,12 +40,21 @@ class ScriptingUi(ScriptingWidget,ScriptingBase):
         self.script.fullname = self.config.get( self.configname+'.script.fullname' , '' )
         self.script.shortname = os.path.basename(self.script.fullname)
         self.script.code = self.config.get( self.configname+'.script.code' , '' )
-        self.script.scriptingFunctionNames = scriptingFunctionNames
+        self.script.scriptFunctions = scriptFunctions
         self.script.locationSignal.connect( self.onLocation )
         self.script.completed.connect( self.onCompleted )
-        self.textEdit.setupUi(self.textEdit,extraKeywords1=[], extraKeywords2=scriptingFunctionNames)
+        
+        self.textEdit = PulseProgramSourceEdit()
+        self.textEdit.setupUi(self.textEdit,extraKeywords1=[], extraKeywords2=scriptFunctions)
         self.textEdit.textEdit.SendScintilla(QsciScintilla.SCI_SETCARETLINEVISIBLEALWAYS, True)
         self.textEdit.setPlainText(self.script.code)
+        self.splitter.addWidget(self.textEdit)
+        
+        self.console = QtGui.QTextEdit()
+        self.console.setReadOnly(True)
+        self.splitter.addWidget(self.console)
+        self.script.consoleSignal.connect(self.onConsoleSignal)
+        
         #Add only the filename (without the full path) to the combo box
         self.filenameComboBox.addItems( [shortname for shortname, fullname in self.recentFiles.iteritems() if os.path.exists(fullname)] )
 
@@ -51,11 +62,9 @@ class ScriptingUi(ScriptingWidget,ScriptingBase):
         self.actionSave.triggered.connect( self.onSave )
         self.actionReset.triggered.connect(self.onReset)
         self.actionStart.triggered.connect( self.onStart )
+        self.actionPause.triggered.connect( self.onPause )
         self.actionStop.triggered.connect( self.onStop )
         self.actionStopImmediately.triggered.connect(self.onStopImmediately )
-        self.loadButton.setDefaultAction( self.actionOpen )
-        self.saveButton.setDefaultAction( self.actionSave )
-        self.resetButton.setDefaultAction( self.actionReset )
                 
         self.filenameComboBox.currentIndexChanged[str].connect( self.onFilenameChange )
         self.removeCurrent.clicked.connect( self.onRemoveCurrent )
@@ -63,40 +72,57 @@ class ScriptingUi(ScriptingWidget,ScriptingBase):
         self.loadFile(self.script.fullname)
         
         self.setWindowTitle(self.configname)
-        self.setWindowIcon(QIcon(":/other/icons/Terminal-icon.png"))
+        self.setWindowIcon(QtGui.QIcon(":/other/icons/Terminal-icon.png"))
+        self.statusLabel.setText("Idle")
 
-
-    @pyqtSlot(int)        
+    @QtCore.pyqtSlot(int)        
     def onLocation(self, currentLine):
         """Mark where the script currently is"""
-        #highlight line that we're on
-        self.textEdit.textEdit.markerDeleteAll()
-        self.textEdit.textEdit.markerAdd(currentLine-1, self.textEdit.textEdit.ARROW_MARKER_NUM)
-        self.textEdit.textEdit.setCaretWidth(0)
-        self.textEdit.textEdit.setCaretLineBackgroundColor(QColor(0xd0, 0xff, 0xd0))
-        self.textEdit.textEdit.setCursorPosition(currentLine-1, 0)
+        if currentLine >= 0:
+            self.textEdit.textEdit.markerDeleteAll()
+            self.textEdit.textEdit.markerAdd(currentLine-1, self.textEdit.textEdit.ARROW_MARKER_NUM)
+            self.textEdit.textEdit.setCursorPosition(currentLine-1, 0)
         
-    @pyqtSlot()
+    @QtCore.pyqtSlot()
     def onStart(self):
         logger = logging.getLogger(__name__)
-        self.textEdit.setDisabled(True)
-        self.filenameComboBox.setDisabled(True)
+        self.statusLabel.setText("Script running")
         self.onSave()
+        self.enableScriptChange(False)
+        runningScript = self.script.fullname #@UnusedVariable
         self.script.start()
-#         try:
-#             execfile(self.script.fullname)
-#         except ScriptException as e:
-#             logger.error("script failed: {0}".format(e))
-            #show on script console
     
-    @pyqtSlot()
+    def enableScriptChange(self, enabled):
+        """Enable or disable any changes to script"""
+        color = QtGui.QColor("#ffe4e4") if enabled else QtGui.QColor(0xd0, 0xff, 0xd0)
+        width = 1 if enabled else 0
+        self.textEdit.textEdit.setCaretWidth(width)
+        self.textEdit.textEdit.setCaretLineBackgroundColor(color)
+        self.textEdit.setDisabled(not enabled)
+        self.filenameComboBox.setDisabled(not enabled)
+        self.removeCurrent.setDisabled(not enabled)
+        self.actionOpen.setEnabled(enabled)
+        self.actionSave.setEnabled(enabled)
+        self.actionReset.setEnabled(enabled)
+        self.actionNew.setEnabled(enabled)
+    
+    @QtCore.pyqtSlot()
     def onCompleted(self):
-        self.textEdit.textEdit.setCaretWidth(1)
-        self.textEdit.textEdit.setCaretLineBackgroundColor(QColor("#ffe4e4"))
+        self.statusLabel.setText("Idle")
         self.textEdit.textEdit.markerDeleteAll()
-        self.textEdit.setDisabled(False)
-        self.filenameComboBox.setDisabled(False)
-   
+        runningScript = '' #@UnusedVariable
+        self.enableScriptChange(True)
+        
+    @QtCore.pyqtSlot(str, bool)
+    def onConsoleSignal(self, message, noError):
+        self.console.moveCursor(QtGui.QTextCursor.End)
+        textColor = "black" if noError else "red"
+        self.console.insertHtml(QtCore.QString('<font color='+textColor+'>'+message+'</font><br>'))
+
+    @QtCore.pyqtSlot()
+    def onPause(self):
+        pass
+    
     def onStop(self):
         pass
     
@@ -114,7 +140,7 @@ class ScriptingUi(ScriptingWidget,ScriptingBase):
                         w.setCurrentIndex( self.filenameComboBox.findText( shortname ))
     
     def onLoad(self):
-        fullname = str(QFileDialog.getOpenFileName(self, 'Open Scripting file',ProjectSelection.configDir()+'\\Scripts','Python scripts (*.py *.pyw)'))
+        fullname = str(QtGui.QFileDialog.getOpenFileName(self, 'Open Scripting file',ProjectSelection.configDir()+'\\Scripts','Python scripts (*.py *.pyw)'))
         if fullname!="":
             self.loadFile(fullname)
            
@@ -164,7 +190,7 @@ class ScriptingUi(ScriptingWidget,ScriptingBase):
             self.move(pos)
         if size:
             self.resize(size)
-        QDialog.show(self)
+        QtGui.QDialog.show(self)
         self.isShown = True
 
     def onClose(self):
