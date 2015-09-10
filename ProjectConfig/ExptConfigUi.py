9c4d6fdca741c5d7d1faf06280be28af32c9c108
Jonathan Mizrahi <jmizrahi@umd.edu>
2015-09-10 16:22:23 -0400

    Moved things around, added global project variables

---
+++ ProjectConfig/ExptConfigUi.py
@@ -0,0 +1,98 @@
+"""
+Created on 10 Sep 2015 at 11:08 AM
+
+@author: jmizrahi
+"""
+
+import os.path
+import sys
+import logging
+from PyQt4 import QtGui, QtCore
+import PyQt4.uic
+from datetime import datetime
+
+uiPath = os.path.join(os.path.dirname(__file__), '..', 'ui/ExptConfig.ui')
+Form, Base = PyQt4.uic.loadUiType(uiPath)
+
+class ExptConfigUi(Base,Form):
+    """Class for configuring an experiment"""
+    def __init__(self, project):
+        Base.__init__(self)
+        Form.__init__(self)
+        self.project = project
+        self.projectConfig = project.projectConfig
+        self.setupUi(self)
+
+    def setupUi(self, parent):
+        """setup the dialog box ui"""
+        super(ExptConfigUi,self).setupUi(parent)
+        self.infoLabel.setText(
+            "This dialog box overwrites the configuration file {0}.".format(
+                self.project.exptConfigFilename))
+        self.setBaseDir()
+        self.defaultCheckBox.setChecked(not self.projectConfig['showGui'])
+        self.populateProjectList()
+        self.changeBaseDirectory.clicked.connect(self.onChangeBaseDirectory)
+        self.createButton.clicked.connect(self.onCreate)
+
+    def setBaseDir(self):
+        """Get a valid base directory"""
+        logger = logging.getLogger(__name__)
+        if not os.path.exists(self.projectConfig['baseDir']):
+            baseDir = str(QtGui.QFileDialog.getExistingDirectory(self,
+                                                                 'Please select a base directory for projects',
+                                                                 os.path.expanduser('~')
+                                                                ))
+            if not os.path.exists(baseDir):
+                message = "Valid base directory for projects must be specified for IonControl program to run"
+                logger.exception(message)
+                sys.exit(message)
+            else:
+                self.projectConfig['baseDir'] = baseDir
+        self.baseDirectoryEdit.setText(self.projectConfig['baseDir'])
+
+    def onCreate(self):
+        """Create a new project folder"""
+        name = str(self.newProjectName.text())
+        projectDir = os.path.join(self.projectConfig['baseDir'], name)
+        if not os.path.exists(projectDir):
+            os.makedirs(projectDir)
+            tagFilename = os.path.join(projectDir, projectTag)
+            with open(tagFilename, 'w') as f:
+                newFileText = 'project {0} created {1}'.format(name, datetime.now())
+                f.write(newFileText)
+        item = QtGui.QListWidgetItem(name)
+        self.projectList.addItem(item)
+        self.projectList.setCurrentItem(item)
+        self.newProjectName.clear()
+
+    def populateProjectList(self):
+        self.projectList.clear()
+        projects = [name for name in os.listdir(self.projectConfig['baseDir']) if os.path.exists(os.path.join(self.projectConfig['baseDir'], name, projectTag))]
+        self.projectList.addItems(projects)
+        matches = self.projectList.findItems(self.projectConfig['name'], QtCore.Qt.MatchExactly)
+        if matches:
+            self.projectList.setCurrentItem(matches[0])
+        elif projects:
+            self.projectList.setCurrentRow(0)
+
+    def onChangeBaseDirectory(self):
+        baseDir = QtGui.QFileDialog.getExistingDirectory(self)
+        if baseDir:
+            self.projectConfig['baseDir'] = str(baseDir)
+            self.baseDirectoryEdit.setText(baseDir)
+            self.populateProjectList()
+
+    def accept(self):
+        selectedProject = self.projectList.currentItem()
+        if selectedProject: #something is selected
+            self.projectConfig['showGui'] = not self.defaultCheckBox.isChecked()
+            self.projectConfig['name'] = str(selectedProject.text())
+            Base.accept(self)
+        else: #if nothing is selected, equivalent to clicking cancel
+            Base.reject(self)
+
+    def reject(self):
+        message = "Project must be selected for IonControl program to run"
+        logging.getLogger(__name__).exception(message)
+        sys.exit(message)
--- ProjectConfig/Project.py
+++ ProjectConfig/Project.py
@@ -4,123 +4,107 @@ Created on 09 Sep 2015 at 2:26 PM
 @author: jmizrahi
 """
 
+import __main__
 import os.path
 import sys
 import yaml
+import logging
 from PyQt4 import QtGui, QtCore
 import PyQt4.uic
-ProjectConfigUiPath = os.path.join(os.path.dirname(__file__), '..', 'ui/ProjectConfig.ui')
-ProjectConfigUiForm, ProjectConfigUiBase = PyQt4.uic.loadUiType(ProjectConfigUiPath)
+from datetime import datetime
+from persist.DatabaseConnectionSettings import DatabaseConnectionSettings
+from ProjectConfigUi import ProjectConfigUi, projectTag
+from ExptConfigUi import ExptConfigUi
+
+projectName=None
+projectDir=None
+dbConnection=None
 
 class Project(object):
     def __init__(self):
         """initialize a project by loading in the project config information"""
+        logger = logging.getLogger(__name__)
         mainDir = os.path.join(os.path.dirname(__file__), '..') #main IonControl directory
         filename = 'config/ProjectConfig.yml' #relative path to config file
+        self.projectConfig = {'baseDir':'', 'name':'', 'showGui':True} #default values
+        self.exptConfig = {'showGui':True}
         self.projectConfigFilename = os.path.realpath(os.path.join(mainDir, filename)) #absolute path to config file
 
-        #load in config information
-        with open(self.projectConfigFilename, 'r') as f:
-            self.__dict__.update(yaml.load(f))
+        #Load in the project config information
+        if os.path.exists(self.projectConfigFilename):
+            with open(self.projectConfigFilename, 'r') as f:
+                try:
+                    yamldata = yaml.load(f)
+                    self.projectConfig = yamldata
+                except yaml.scanner.ScannerError:
+                    pass #leave defaults if the file is improperly formatted
 
-        if not os.path.exists(self.baseDir): #If the baseDir doesn't exist, we have to use the GUI
-            self.showProjectConfigGui = True
-        elif not self.name: #If no project name is specified, we have to use the GUI
-            self.showProjectConfigGui = True
-        if self.showProjectConfigGui:
-            ui = ProjectSelectionUi(self)
+        #If the baseDir doesn't exist or no project is specified, we have to use the GUI
+        if not os.path.exists(self.projectConfig['baseDir']) or not self.projectConfig['name']:
+            self.projectConfig['showGui'] = True
+
+        if self.projectConfig['showGui']:
+            ui = ProjectConfigUi(self)
             ui.show()
-            accept = ui.exec_()
-            if not accept:
-                sys.exit("Project must be selected for IonControl program to run")
-            else: #overwrite the config file with the values just set
-                with open(self.projectConfigFilename, 'w') as f:
-                    yaml.dump({'showProjectConfigGui':self.showProjectConfigGui,
-                               'baseDir':self.baseDir,
-                               'name':self.name}, f, default_flow_style=False)
+            ui.exec_()
+            with open(self.projectConfigFilename, 'w') as f: #save information from GUI to file
+                yaml.dump(self.projectConfig, f, default_flow_style=False)
 
-        self.projectDir = os.path.join(self.baseDir, self.name)
+        self.projectDir = os.path.join(self.projectConfig['baseDir'], self.projectConfig['name'])
         if not os.path.exists(self.projectDir):
             os.makedirs(self.projectDir)
+            tagFilename = os.path.join(self.projectDir, projectTag)
+            message = 'project {0} created {1}'.format(self.projectConfig['name'], datetime.now())
+            with open(tagFilename, 'w') as f:
+                f.write(message)
+            logger.info(message)
 
-        #load in experiment configuration information
-        self.exptConfigFilename = os.path.join(self.projectDir, 'ExptConfig.yml')
- #       if os.path.exists(self.exptConfigFilename):
+        self.guiConfigDir = os.path.join(self.projectDir, '.gui-config')
+        if not os.path.exists(self.guiConfigDir):
+            os.makedirs(self.guiConfigDir)
+        scriptname,_ = os.path.splitext( os.path.basename(__main__.__file__))
+        self.guiConfigFile = os.path.join( self.guiConfigDir, scriptname+".config.db" )
+        self.configDir = os.path.join(self.projectDir, 'config')
+        if not os.path.exists(self.configDir):
+            os.makedirs(self.configDir)
 
+        #Load in the experiment config information
+        self.exptConfigFilename = os.path.realpath(os.path.join(self.projectDir, 'config/ExptConfig.yml'))
+        if os.path.exists(self.exptConfigFilename):
+            with open(self.exptConfigFilename, 'r') as f:
+                try:
+                    yamldata = yaml.load(f)
+                    self.exptConfig = yamldata
+                except yaml.scanner.ScannerError:
+                    pass #leave defaults if the file is improperly formatted
 
-class ProjectSelectionUi(ProjectConfigUiBase,ProjectConfigUiForm):
-    """Class for selecting a project"""
-    def __init__(self, project):
-        ProjectConfigUiBase.__init__(self)
-        ProjectConfigUiForm.__init__(self)
-        self.project = project
-        self.setupUi(self)
+        if self.exptConfig['showGui']: #TODO: THIS DOESN'T WORK YET
+            ui = ExptConfigUi(self)
+            ui.show()
+            ui.exec_()
+            with open(self.exptConfigFilename, 'w') as f: #save information from GUI to file
+                yaml.dump(self.exptConfig, f, default_flow_style=False)
 
-    def setupUi(self, parent):
-        """setup the dialog box ui"""
-        super(ProjectSelectionUi,self).setupUi(parent)
-        self.infoLabel.setText(
-            "This dialog box overwrites the configuration file {0}.".format(
-                self.project.projectConfigFilename))
-        self.setBaseDir()
-        self.defaultCheckBox.setChecked(not self.project.showProjectConfigGui)
-        self.populateProjectList()
-        self.changeBaseDirectory.clicked.connect(self.onChangeBaseDirectory)
-        self.createButton.clicked.connect(self.onCreate)
+        self.dbConnection = DatabaseConnectionSettings(**self.exptConfig['databaseConnection'])
 
-    def setBaseDir(self):
-        """Get a valid base directory"""
-        if not os.path.exists(self.project.baseDir):
-            baseDir = str(QtGui.QFileDialog.getExistingDirectory(self,
-                                                                 'Please select a base directory for projects',
-                                                                 os.path.expanduser('~')
-                                                                ))
-            if not os.path.exists(baseDir):
-                sys.exit("Valid base directory for projects must be specified for IonControl program to run")
-            else:
-                self.project.baseDir = baseDir
-        self.baseDirectoryEdit.setText(self.project.baseDir)
+        self.setGlobalProjectVars()
 
-    def onCreate(self):
-        """Create a new project folder"""
-        name = str(self.newProjectName.text())
-        projectDir = os.path.join(self.project.baseDir, name)
-        if not os.path.exists(projectDir):
-            os.makedirs(projectDir)
-        item = QtGui.QListWidgetItem(name)
-        self.projectList.addItem(item)
-        self.projectList.setCurrentItem(item)
-        self.newProjectName.clear()
+    @property
+    def name(self):
+        return self.projectConfig['name']
 
-    def populateProjectList(self):
-        self.projectList.clear()
-        projects = [name for name in os.listdir(self.project.baseDir) if os.path.isdir(os.path.join(self.project.baseDir, name))]
-        self.projectList.addItems(projects)
-        matches = self.projectList.findItems(self.project.name, QtCore.Qt.MatchExactly)
-        if matches:
-            self.projectList.setCurrentItem(matches[0])
-        elif projects:
-            self.projectList.setCurrentRow(0)
+    def __str__(self):
+        return self.name
 
-    def onChangeBaseDirectory(self):
-        baseDir = QtGui.QFileDialog.getExistingDirectory(self)
-        if baseDir:
-            self.project.baseDir = str(baseDir)
-            self.baseDirectoryEdit.setText(baseDir)
-            self.populateProjectList()
-
-    def accept(self):
-        selectedProject = self.projectList.currentItem()
-        if selectedProject: #something is selected
-            self.project.showProjectConfigGui = not self.defaultCheckBox.isChecked()
-            self.project.name = str(selectedProject.text())
-            ProjectConfigUiBase.accept(self)
-        else: #if nothing is selected, equivalent to clicking cancel
-            ProjectConfigUiBase.reject(self)
+    def setGlobalProjectVars(self):
+        global projectDir
+        global dbConnection
+        global projectName
+        projectName = self.name
+        projectDir = self.projectDir
+        dbConnection = self.dbConnection
 
 if __name__ == '__main__':
     import sys
     app = QtGui.QApplication(sys.argv)
     project = Project()
-
-
